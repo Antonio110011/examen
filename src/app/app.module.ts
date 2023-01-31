ce1610a7d50a125541f9240c57fb61aa76d552eb
unknown <juanantonio.10.11@gmail.com>
2023-01-30 20:21:21 -0600

    primer subida

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BodyComponent } from './components/body/body.component';
+import { GridComponent } from './components/grid/grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BodyComponent,
+    GridComponent
   ],
   imports: [
     BrowserModule
